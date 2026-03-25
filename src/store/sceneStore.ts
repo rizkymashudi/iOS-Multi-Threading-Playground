16d4d0ffbcca4dd7180744a7f8d70717046a6599
rizky mashudi <v00029222@danamon.co.id>
2026-03-25 15:52:55 +0700

    feat(stores): add sidebar collapsed state to sceneStore
    - sidebarCollapsed boolean and toggleSidebar action

--- src/store/sceneStore.ts
+++ src/store/sceneStore.ts
@@ -4,9 +4,13 @@ import type { SceneId } from '../types/scene'
 interface SceneStore {
   activeSceneId: SceneId
   setScene: (id: SceneId) => void
+  sidebarCollapsed: boolean
+  toggleSidebar: () => void
 }
 
 export const useSceneStore = create<SceneStore>((set) => ({
   activeSceneId: 'gcd',
   setScene: (id) => set({ activeSceneId: id }),
+  sidebarCollapsed: false,
+  toggleSidebar: () => set((s) => ({ sidebarCollapsed: !s.sidebarCollapsed })),
 }))
